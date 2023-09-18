ca7284cdf239d9c475a8b12502643e449d4a65e7
chetandawani <142264155+chetandawani@users.noreply.github.com>
2023-09-19 02:12:43 +0530

    Program of Strong Password Generation
    In this file There is a Project Code which is based upon a GUI Auto Password Generation Code which has some objective that in this era in which hacker has a easy unauthorized Access to your Accounts which lead to huge financial losses sometimes for you so  therefore This Code will help you to create a very very Strong Password which is hard to crack by a hacker with a variety of Combinations of the characters

---
+++ Pswd-Genration_T3.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 17 13:05:55 2023
+
+@author: DELL
+"""
+import tkinter as tk
+from tkinter import messagebox
+import random
+import string
+
+def generate_password():
+    password_length = int(length_entry.get())
+    if password_length < 8:
+        messagebox.showwarning("Warning:", "Password length should be atleast 8 characters.")
+        return
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(password_length))
+    password_entry.delete(0, tk.END)
+    password_entry.insert(0, password)
+
+def copy_to_clipboard():
+    password = password_entry.get()
+    root.clipboard_clear()
+    root.clipboard_append(password)
+    messagebox.showinfo("Successfully", "Your Password is copied to clipboard !")
+
+# Create the main application window
+root = tk.Tk()
+root.title("Password Generator by Chetan Dawani")
+root.geometry('500x400')
+root.resizable(10, 10)
+root.config(bg="Blue")
+
+# Create widgets
+length_label = tk.Label(root, text="Password Length:", font=('Algerian', 14, 'bold'))
+length_label.pack(pady=20)
+length_entry = tk.Entry(root)
+length_entry.pack(pady=28)
+
+generate_button = tk.Button(root, text="Generate Password", command=generate_password, font=('Algerian', 14, 'bold'))
+generate_button.pack(pady=20)
+
+password_entry = tk.Entry(root, show="")
+password_entry.pack(pady=20)
+
+copy_button = tk.Button(root, text="Copy to your Clipboard", command=copy_to_clipboard, font=('Algerian', 14, 'bold'))
+copy_button.pack(pady=40)
+
+root.mainloop()
